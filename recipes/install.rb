0f63176a8df82c0de5a70e86866e45e5f11a38be
Baptiste Courtois <b.courtois@criteo.com>
2015-01-25 14:44:38 +0100

    Support Windows Server 2012 properly

--- recipes/install.rb
+++ recipes/install.rb
@@ -20,59 +20,100 @@
 # WSUS is a windows only feature
 return unless platform?('windows')
 
-# WSUS 3.0 SP2 requires some IIS features: http://technet.microsoft.com/en-us/library/dd939916.aspx
-features = %w(
-  IIS-WebServerRole
-  IIS-WebServer
-  IIS-ApplicationDevelopment
-  IIS-ISAPIFilter
-  IIS-ISAPIExtensions
-  IIS-NetFxExtensibility
-  IIS-ASPNET
-  IIS-WindowsAuthentication
-  IIS-HttpCompressionDynamic
-  IIS-IIS6ManagementCompatibility
-  IIS-WMICompatibility
-  IIS-Metabase
-  IIS-LegacyScripts
-)
-
-# IIS 6 SnapIn is not compatible with core version
-features << 'IIS-LegacySnapIn' unless Chef::ReservedNames::Win32::Version.new.core?
-features.each do |feature_name|
-  windows_feature feature_name do
-    action                                       :install
-  end
-end
-
-include_recipe 'wsus-server::report_viewer'
-
 setup_conf = node['wsus_server']['setup']
-package_info = node['wsus_server']['package']
 
-setup_options = package_info['options']
-setup_options << ' ENABLE_INVENTORY=' + (setup_conf['enable_inventory'] ? '1' : '0')
-setup_options << ' MU_ROLLUP=' + (setup_conf['join_improvement_program'] ? '1' : '0')
-setup_options << ' DEFAULT_WEBSITE=' + (setup_conf['use_default_website'] ? '1' : '0')
-setup_options << ' FRONTEND_SETUP=1 CREATE_DATABASE=0'                            if setup_conf['frontend_setup']
+setup_options = ''
 setup_options << " SQLINSTANCE_NAME=\"#{setup_conf['sqlinstance_name']}\""        if setup_conf['sqlinstance_name']
 
 if setup_conf['content_dir']
   setup_options << " CONTENT_LOCAL=1 CONTENT_DIR=\"#{setup_conf['content_dir']}\""
-  directory setup_conf['content_dir'] { recursive true }
+
+  directory setup_conf['content_dir'] do
+    recursive true
+  end
 end
 
-if setup_conf['wyukon_data_dir']
-  setup_options << " WYUKON_DATA_DIR=\"#{setup_conf['wyukon_data_dir']}\""
-  directory setup_conf['wyukon_data_dir'] { recursive true }
-end
+require 'chef/win32/version'
+if node['platform_version'].to_f >= 6.2
 
-windows_package package_info['name'] do
-  action                                         :install
-  installer_type                                 :custom
-  options                                        setup_options
-  source                                         package_info['source']
-  checksum                                       package_info['checksum']
+  windows_feature 'UpdateServices' do
+    action         :install
+    all            true
+  end
+
+  windows_feature 'UpdateServices-UI' do
+    action         :install
+    all            true
+  end
+
+  windows_feature 'UpdateServices-Database' do
+    action         setup_conf['sqlinstance_name'] ? :install : :remove
+    all            true
+  end
+
+  windows_feature 'UpdateServices-WidDatabase' do
+    action         setup_conf['sqlinstance_name'] ? :remove : :install
+    all            true
+  end
+
+  guard_file = ::File.join(Chef::Config['file_cache_path'], 'wsus_postinstall')
+  execute 'WSUS PostInstall' do
+    command        "WsusUtil.exe PostInstall #{setup_options}"
+    cwd            'C:\Program Files\Update Services\Tools'
+    not_if { ::File.exist?(guard_file) && ::File.read(guard_file) == setup_options }
+  end
+
+  file guard_file do
+    path           guard_file
+    content        setup_options
+  end
+else
+  setup_options << ' ENABLE_INVENTORY=' + (setup_conf['enable_inventory'] ? '1' : '0')
+  setup_options << ' MU_ROLLUP=' + (setup_conf['join_improvement_program'] ? '1' : '0')
+  setup_options << ' DEFAULT_WEBSITE=' + (setup_conf['use_default_website'] ? '1' : '0')
+  setup_options << ' FRONTEND_SETUP=1 CREATE_DATABASE=0' if setup_conf['frontend_setup']
+
+  if setup_conf['wyukon_data_dir']
+    setup_options << " WYUKON_DATA_DIR=\"#{setup_conf['wyukon_data_dir']}\""
+    directory setup_conf['wyukon_data_dir'] { recursive true }
+  end
+
+  # WSUS 3.0 SP2 requires some IIS features: http://technet.microsoft.com/en-us/library/dd939916.aspx
+  features = %w(
+    IIS-WebServerRole
+    IIS-WebServer
+    IIS-ApplicationDevelopment
+    IIS-ISAPIFilter
+    IIS-ISAPIExtensions
+    IIS-NetFxExtensibility
+    IIS-ASPNET
+    IIS-WindowsAuthentication
+    IIS-HttpCompressionDynamic
+    IIS-IIS6ManagementCompatibility
+    IIS-WMICompatibility
+    IIS-Metabase
+    IIS-LegacyScripts
+  )
+
+  # IIS 6 SnapIn is not compatible with core version
+  features << 'IIS-LegacySnapIn' unless Chef::ReservedNames::Win32::Version.new.core?
+  features.each do |feature_name|
+    windows_feature feature_name do
+      action       :install
+    end
+  end
+
+  include_recipe 'wsus-server::report_viewer'
+
+  package_info = node['wsus_server']['package']
+
+  windows_package package_info['name'] do
+    action         :install
+    installer_type :custom
+    options        "#{package_info['options']} #{setup_options}"
+    source         package_info['source']
+    checksum       package_info['checksum']
+  end
 end
 
 # Wsus does not need configuration when setup as frontend server
